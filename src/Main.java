40b30b4cfa0c4949abe7a1cc1a776d2c2f702e6c
Tony-StPet <135145428+Tony-StPet@users.noreply.github.com>
2024-09-12 11:28:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
 
         int summ9 = 0;
         int summ99 = 0;
-        System.out.printf("Введите числа : ");
+        int summlimit = 0;
+        System.out.printf("Введите 20 чисел : ");
 
         for (int count = 0; count != 20; count++) {
             int num = scanner.nextInt();
@@ -13,10 +14,13 @@ public class Main {
                 summ9++;
             else if (num < 100)
                 summ99++;
+            else
+                summlimit++;
         }
         System.out.println("Вы ввели 20 значений! Посчитаем результат");
 
         System.out.println("Сумма чисел  от 0 до 9 = " + summ9);
         System.out.println("Сумма чисел от 10 до 99 = " + summ99);
+        System.out.println("Сумма чисел не в пределах = " + summlimit);
     }
 }
